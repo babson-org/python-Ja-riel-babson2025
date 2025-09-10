97850993c0d59e6e4bc1ed72fec7353878e8162b
Ja-riel-babson2025 <jbailey1@babson.edu>
2025-09-10 14:42:33 +0000

    update tempCodeRunnerFile.py

--- classes/week02/wednesday/exercises/tempCodeRunnerFile.py
+++ classes/week02/wednesday/exercises/tempCodeRunnerFile.py
@@ -1,12 +1,16 @@
-def greet(name):
-    # Capitalize the name before greeting
-    name = input("Please enter your name: ")
-    name = name.capitalize()
-    """
-    The function prints a greeting.
-    By taking the user's name,
-    capitalizing it, then
-    including it in the message.
-    """
-    print(f"Hello, {name}!")
-greet(" ")
+from classes.week00.second_class.utils import clear_screen
+'''
+#13 - Conditional Logic
+Ask the user for a number and print whether it is positive, negative, or zero.
+'''
+# enter code here
+num = int(input("Enter a number please: "))
+if num > 0:
+    print("The number entered is POSITIVE.")
+elif num < 0:
+    print("The number entered is NEGATIVE.")
+else:
+    print("The number is ZERO.")
+
+pause=input('pause')
+clear_screen()
