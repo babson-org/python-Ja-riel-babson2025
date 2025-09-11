4e081cbc7f62b326e294557ef88fb4d1734513e4
Ja-riel-babson2025 <jbailey1@babson.edu>
2025-09-11 01:42:47 +0000

    update lab1.py

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -62,7 +62,7 @@ def caesar_cipher():
     Provide options to encrypt or decrypt the text using a Caesar cipher.
     """
     # TODO: Get user input text
-    text = input("Enter text: ")
+    txt = input("Enter text: ")
 
     # TODO: Get shift value
     shift = int(input("Enter shift value (integer): "))
@@ -101,3 +101,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+alphebet = []
+for idx in range(97, 97+26):
+    alphebet.append(chr(idx))
+print(alphebet)
+shift = int(input('Enter shift'))
+    check int>0<26
+shift = 8
+shift.array = [None] * 26
+encrypt = ''
+for idx in range(26):
+    idx = alphebet.idex(char)
+new_index = (idx-shift) % 26
+encrypt += shift_alphebet[new_index]
---
+++ labs/lab1/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+alphebet = []
+for idx in range(97, 97+26):
+    alphebet.append(chr(idx))
+print(alphebet)
+shift = int(input('Enter shift'))
+    check int>0<26
+shift = 8
+shift.array = [None] * 26
+encrypt = ''
+for idx in range(26):
+    idx = alphebet.idex(char)
+new_index = (idx-shift) % 26
+encrypt += shift_alphebet[new_index]
