93f2dd5ebe5778cf73b4044e5f31e270befa4608
Ja-riel-babson2025 <jbailey1@babson.edu>
2025-09-12 21:19:34 +0000

    update lab1_update.py and tempCodeRunnerFile.py

--- labs/lab1/lab1_update.py
+++ labs/lab1/lab1_update.py
@@ -42,7 +42,6 @@ def draw_diamond():
 
 # Uncomment to test Part 1
 # draw_diamond()
-
 ## ENSURES THAT THE USER ENTERS A NUMBERIC VALUE AND NOT WORDS/INVALID VALUES
 while True: 
     try:
@@ -50,32 +49,32 @@ while True:
         if height % 2 == 1:
             break
         else:
-            print('The number must be odd! Please try again')
+            print('The number must be odd! Please try again: ')
     except ValueError:
-        print('Invalid value. Please try again!')
-    print("you have some work todo!, draw_diamond")
+        print('Invalid value! Please try again!')
 
 ## DEFINES THE MIDDLE OF DIAMOND PARAMETERS FOR THE WIDTH OF THE DIAMOND
 middle = height // 2
 
 ## OUTLINES SPACES FOR THE LENGTH OF THE STARS FOR BOTH SPACES 
 ### BEFORE AND BETWEEN THE STARTING POINT OF THE DIAMOND
-for idx in range (1, middle, -1, -1):
+for idx in range (middle, -1, -1):
     before = " " * idx
-    between = " " * ((middle - idx) *2-1)
-if (middle * 2-1 == -1):
+    between = " " * ((middle - idx) * 2-1)
+if (middle - idx * 2-1 == -1):
     print(before + "*")
 else:
     print(before + "*" + between + "*")
 
-#### DEFINES THE BOTTOM HALD OF THE DIAMOND
+#### DEFINES THE BOTTOM HALf OF THE DIAMOND
 for idx in range (1, middle+1):
     before = " " * idx
     between = " " * ((middle - idx) *2-1)
-if (middle * 2-1 == -1):
-    print(before + "*")
+    if (middle-idx * 2-1 == -1):
+        print(before + "*")
 else:
     print(before + "*" + between + "*")
+
 
 # ==============================
 # Part 2: Count Letters, Words, and Sentences
@@ -117,15 +116,13 @@ txt = input('Enter text + punctuation please: ').strip()
 for char in txt:
     if char.isalpha(): 
         Letters += 1
-    elif char == '': 
+    elif char == ' ': 
         Words += 1
     elif char in ('.', '!', '?'): 
         Sentences +=1
 print('letters', Letters)
 print('words', Words)
 print('sentences', Sentences)
-
-
 
 
 # ==============================
--- labs/lab1/tempCodeRunnerFile.py
+++ labs/lab1/tempCodeRunnerFile.py
@@ -1,15 +1,31 @@
-Letters = 0
-Words = 1
-Sentences = 0
+while True: 
+    try:
+        height = int(input('Enter and odd number for the diamond height: '))
+        if height % 2 == 1:
+            break
+        else:
+            print('The number must be odd! Please try again: ')
+    except ValueError:
+        print('Invalid value. Please try again!')
 
-txt = input('Enter text please: ').strip()
-for char in txt:
-    if char.isalpha(): 
-        Letters += 1
-    elif char == '': 
-        Words += 1
-    elif char in ('.', '!', '?'): 
-        Sentences +=1
-print('letters', Letters)
-print('words', Words)
-print('sentences', Sentences)
+## DEFINES THE MIDDLE OF DIAMOND PARAMETERS FOR THE WIDTH OF THE DIAMOND
+middle = height // 2
+
+## OUTLINES SPACES FOR THE LENGTH OF THE STARS FOR BOTH SPACES 
+### BEFORE AND BETWEEN THE STARTING POINT OF THE DIAMOND
+for idx in range (middle, -1, -1):
+    before = " " * idx
+    between = " " * ((middle - idx) * 2-1)
+if (middle - idx * 2-1 == -1):
+    print(before + "*")
+else:
+    print(before + "*" + between + "*")
+
+#### DEFINES THE BOTTOM HALf OF THE DIAMOND
+for idx in range (1, middle+1):
+    before = " " * idx
+    between = " " * ((middle - idx) *2-1)
+    if (middle-idx * 2-1 == -1):
+        print(before + "*")
+else:
+    print(before + "*" + between + "*")
