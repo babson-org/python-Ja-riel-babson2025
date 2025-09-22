83188f36e072681714252e8290c3c6f6c50d17d5
Ja-riel-babson2025 <jbailey1@babson.edu>
2025-09-22 15:17:24 +0000

    update recursion.py and synatic_candy.ipynb

--- classes/week04/monday/recursion.py
+++ classes/week04/monday/recursion.py
@@ -1,9 +1,17 @@
 def myPrint(n):
     
-    print("hello", n)       
+    print("hello", n)
+    if n == 5: return 'Done'       
     myPrint(n + 1)
 
-    
-    
+print(myPrint(1))
+   # Recurssions need some for of ending/stopping statement 
+
+def recur_sum(n):
+    if n == 0:
+        return 0
+    else: n + recur_sum(n - 1)
+
+print(recur_sum(5))
 
     
