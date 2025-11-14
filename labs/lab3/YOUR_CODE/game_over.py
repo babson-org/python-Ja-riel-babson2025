a67797987d85944c83a8002936e81ede682d5c88
student-name <student@example.com>
2025-11-14 15:51:07 -0500

    Local save before sync (20251114)

--- labs/lab3/YOUR_CODE/game_over.py
+++ labs/lab3/YOUR_CODE/game_over.py
@@ -10,10 +10,10 @@ def game_won(base_board, player_board):
     """Checking if the game is won."""
     for r in range(len(base_board)):
         for c in range(len(base_board[0])):
-            if base_board[r][c] != MINE and player_board[r][c] == globals.HIDDEN:
+            if base_board[r][c] != globals.MINE and player_board[r][c] == "+":
                 return False
         for i in range(len(base_board, row, col)):
-            return base_board[row][col] == MINE  # will detect if the game is lost
+            return base_board[row][col] == globals.MINE  # will detect if the game is lost
         else:
             return True
 
